f210afea9b2d36bf39b2141c7419a14576605183
peterpilgrim <peter.pilgrim@gmail.com>
2016-08-04 15:50:39 +0100

    Case classes and initial unit tests

---
+++ src/main/scala/uk/co/xenonique/agileit/ShoppingCart.scala
@@ -0,0 +1,11 @@
+package uk.co.xenonique.agileit
+
+/**
+  * The type ShoppingCart
+  *
+  * @author Peter Pilgrim (peter)
+  */
+
+class ShoppingCart {
+
+}
---
+++ src/test/scala/uk/co/xenonique/agileit/ShoppingCartSpec.scala
@@ -0,0 +1,28 @@
+package uk.co.xenonique.agileit
+
+import org.junit.runner.RunWith
+import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.junit.JUnitRunner
+
+import scala.collection.immutable.List
+
+/**
+  * The type ShoppingCart
+  *
+  * The @{link ShoppingCartSpec} validates the operation and behaviour of @{link ShoppingCart}
+  * @author Peter Pilgrim (peter)
+  *
+  */
+@RunWith(classOf[JUnitRunner])
+class ShoppingCartSpec extends FlatSpec with Matchers {
+
+  "List of cart items" should "be reversible" in {
+    List(Orange(), Apple(), Apple()).reverse should === ( List(Apple(), Apple(), Orange()))
+  }
+
+}
+
+
+
+
+
